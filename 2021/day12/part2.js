6d8cb9de04645c0893060fdf4dccc6720e278060
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2021-12-12 11:11:04 +0200

    Optimize part 2 of day 12, 2021

--- 2021/day12/part2.js
+++ 2021/day12/part2.js
@@ -1,6 +1,5 @@
 const { readLines } = require("../../utils/input");
 
-console.time("part2")
 const nodes = {};
 
 const addConnectionToNode = (start, end) => {
@@ -28,30 +27,33 @@ readLines().forEach(line => {
   addConnectionToNode(end, start)
 })
 
-const finalPaths = []
+let count = 0;
 const traverse = (nodes, startNode = "start", smallCavesVisited = {}, originalPath = []) => {
-  const start = nodes[startNode]
-  const path = [...originalPath, start.letter];
   if (startNode === "end") {
-    finalPaths.push(path)
+    count++;
     return;
   }
 
+  const start = nodes[startNode]
+
+  // Only add small caves to the path
+  const path = isSmallCave(start.letter) ? [...originalPath, start.letter] : [...originalPath]
+
+  let twoCounts = 0;
+  path.reduce((prev, cave) => {
+    const newCount = (prev[cave] || 0) + 1;
+    if (newCount === 2) twoCounts++;
+    return {
+      ...prev,
+      [cave]: newCount
+    }
+  }, {})
+
+  if (twoCounts >= 2) return;
+
   if (isSmallCave(startNode)) smallCavesVisited[startNode] = (smallCavesVisited[startNode] || 0) + 1
 
-  const connections = start.connections.filter(cave => {
-    const smallCaveCounts = path.reduce((prev, cave) => {
-      if (!isSmallCave(cave) || cave === "start" || cave === "end") return prev;
-
-      return {
-        ...prev,
-        [cave]: (prev[cave] || 0) + 1
-      }
-    }, {})
-
-    const twoCounts = Object.values(smallCaveCounts).reduce((prev, curr) => curr === 2 ? prev + 1 : prev, 0);
-    return twoCounts < 2 && smallCavesVisited[cave] !== 2 && cave !== "start"
-  });
+  const connections = start.connections.filter(cave => smallCavesVisited[cave] !== 2 && cave !== "start");
 
   for (const cave of connections) {
     traverse(nodes, cave, { ...smallCavesVisited }, path)
@@ -59,21 +61,4 @@ const traverse = (nodes, startNode = "start", smallCavesVisited = {}, originalPa
 }
 
 traverse(nodes)
-
-const filtered = finalPaths.filter(path => {
-  const smallCaveCounts = path.reduce((prev, cave) => {
-    if (!isSmallCave(cave) || cave === "start" || cave === "end") return prev;
-
-    return {
-      ...prev,
-      [cave]: (prev[cave] || 0) + 1
-    }
-  }, {})
-
-  const twoCounts = Object.values(smallCaveCounts).reduce((prev, curr) => curr === 2 ? prev + 1 : prev, 0);
-
-  return twoCounts < 2;
-})
-
-console.log(filtered.length)
-console.timeEnd("part2")
+console.log(count)
