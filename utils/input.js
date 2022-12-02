4416c0b627df7d8e095e9d2b032b2c874061f35a
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-12-02 07:35:33 +0200

    Use ES Modules in input.js

--- utils/input.js
+++ utils/input.js
@@ -1,16 +1,11 @@
-const fs = require("fs");
-const { join, dirname } = require("path");
+import fs from "fs"
+import { join, dirname } from "path";
 
-const readLines = (file) => {
+export const readLines = (file) => {
   const inputPath = join(dirname(process.argv[1]), file || "input.txt");
   return fs.readFileSync(inputPath, "utf8").toString().trim().split("\n");
 }
 
-const readNumberList = (file, radix) => {
+export const readNumberList = (file, radix) => {
   return readLines(file).map(line => parseInt(line, radix || 10));
-}
-
-module.exports = {
-  readLines,
-  readNumberList
 }
