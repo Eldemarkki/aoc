3f2e1839acda772c769acd5143d0fd548d3e41b0
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-12-05 09:26:04 +0200

    Day 5, 2022

---
+++ 2022/day5/part1.ts
@@ -0,0 +1,52 @@
+import { readLinesDontTrim } from "../../utils/input";
+const lines = readLinesDontTrim() as string[];
+
+const split = lines.findIndex((line) => line.trim() === "");
+
+function chunkString(str: string, len: number) {
+  const size = Math.ceil(str.length / len)
+  const r = Array(size)
+  let offset = 0
+
+  for (let i = 0; i < size; i++) {
+    r[i] = str.substring(offset, offset + len)
+    offset += len
+  }
+
+  return r
+}
+
+const stacks: Record<number, string[]> = {}
+for (let i = 0; i < split - 1; i++) {
+  const line = lines[i]
+  const crates = chunkString(line, 4).map((crate) => crate.trim().replace("[", "").replace("]", "")) ?? [];
+  for (let j = 0; j < crates.length; j++) {
+    const crate = crates[j];
+    if (!stacks[j]) {
+      stacks[j] = [];
+    }
+    if (crate !== "") {
+      stacks[j] = [crate].concat(stacks[j]);
+    }
+  }
+}
+
+const stackCount = Object.keys(stacks).length
+
+const instructions = lines.slice(split + 1).map((line) => {
+  const splitted = line.trim().split(" ");
+  return [Number(splitted[1]), Number(splitted[3]), Number(splitted[5])];
+});
+
+for (let i = 0; i < instructions.length; i++) {
+  const [count, from, to] = instructions[i];
+  const crates = (stacks[String(from - 1)] ?? []).splice(stacks[String(from - 1)].length - count, count).reverse();
+  stacks[String(to - 1)] = (stacks[String(to - 1)] ?? []).concat(crates);
+}
+
+let answer = "";
+for (let i = 0; i < stackCount; i++) {
+  answer += stacks[i][stacks[i].length - 1];
+}
+
+console.log(answer);
--- utils/input.js
+++ utils/input.js
@@ -6,6 +6,11 @@ export const readLines = (file) => {
   return fs.readFileSync(inputPath, "utf8").toString().trim().split("\n");
 }
 
+export const readLinesDontTrim = (file) => {
+  const inputPath = join(dirname(process.argv[1]), file || "input.txt");
+  return fs.readFileSync(inputPath, "utf8").toString().split("\n");
+}
+
 export const readNumberList = (file, radix) => {
   return readLines(file).map(line => parseInt(line, radix || 10));
 }
