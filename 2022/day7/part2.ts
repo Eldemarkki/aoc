f63f111048ad9ad9b122af4baf1f8cbad7cbd090
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-12-07 16:55:42 +0200

    Day 7, 2022

---
+++ 2022/day7/part2.ts
@@ -0,0 +1,69 @@
+import { readLines } from "../../utils/input";
+const lines = readLines() as string[];
+
+let tree = {};
+
+let currentDir: string[] = [];
+
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i]
+  if (line.startsWith("$")) {
+    const cmd = line.slice(2);
+    const args = cmd.split(" ");
+    if (args[0] === "cd") {
+      if (args[1] === "..") {
+        currentDir.pop();
+      }
+      else {
+        if (args[1] === "/") {
+          currentDir.push("");
+        }
+        else {
+          currentDir.push(args[1]);
+        }
+        tree[currentDir.join("/")] = {}
+      }
+    }
+    else if (args[0] === "ls") {
+      while (lines[i + 1] && !lines[i + 1].startsWith("$")) {
+        i++;
+        const [size, name] = lines[i].split(" ");
+        if (size === "dir") {
+          tree[[...currentDir, name].join("/")] = {};
+        }
+        else {
+          const sizeNum = parseInt(size);
+          tree[[...currentDir, name].join("/")] = sizeNum;
+        }
+      }
+    }
+  }
+}
+
+const calculateSum = (obj: any, keyStart: string) => {
+  let sum = 0;
+  for (const key in obj) {
+    if (key.startsWith(keyStart)) {
+      const value = obj[key];
+      if (typeof value === "number") {
+        sum += value;
+      }
+    }
+  }
+  return sum;
+}
+
+const folderKeys = Object.keys(tree).filter(k => typeof tree[k] === "object").concat("/")
+
+const currentUnusedSpace = 70000000 - calculateSum(tree, "");
+
+const howMuchFreeIncrease = {}
+for (const excludedKey of folderKeys) {
+  howMuchFreeIncrease[excludedKey] = calculateSum(tree, excludedKey);
+}
+
+const x = Object.values(howMuchFreeIncrease)
+  .map(val => Number(val) + currentUnusedSpace)
+  .filter(v => v >= 30000000)
+  .sort((a, b) => a - b)[0] - currentUnusedSpace
+console.log(x)
