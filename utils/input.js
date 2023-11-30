22163482d67b00cab637e42a480b379500847e6b
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-11-30 23:47:24 +0200

    feat: disable Github Copilot and add JSDoc types to input utils

--- utils/input.js
+++ utils/input.js
@@ -1,16 +1,29 @@
 import fs from "fs"
 import { join, dirname } from "path";
 
+/**
+ * 
+ * @param {string} [file] 
+ */
 export const readLines = (file) => {
   const inputPath = join(dirname(process.argv[1]), file || "input.txt");
   return fs.readFileSync(inputPath, "utf8").toString().trim().split("\n");
 }
 
+/**
+ * 
+ * @param {string} [file]
+ */
 export const readLinesDontTrim = (file) => {
   const inputPath = join(dirname(process.argv[1]), file || "input.txt");
   return fs.readFileSync(inputPath, "utf8").toString().split("\n");
 }
 
+/**
+ * 
+ * @param {string} file
+ * @param {number} radix
+ */
 export const readNumberList = (file, radix) => {
   return readLines(file).map(line => parseInt(line, radix || 10));
 }
