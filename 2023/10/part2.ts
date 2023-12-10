4c2bd9e39ab8e47a2163861581df99eac50fedb5
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-12-10 11:50:36 +0200

    Make day 10 part 2 faster

--- 2023/10/part2.ts
+++ 2023/10/part2.ts
@@ -1,13 +1,15 @@
 import { readLines } from "../../utils/input";
 
 const lines = readLines("input.txt");
-type TileType = "S" | "-" | "7" | "F" | "L" | "J" | "|";
+
+type Point = {
+  x: number;
+  y: number;
+};
 
 type Tile = {
   x: number;
   y: number;
-  type: TileType;
-  isStart: boolean;
   connections: Set<string>;
 };
 
@@ -113,8 +115,6 @@ lines.forEach((line, y) => {
     uncheckedMap.set(x + "-" + y, {
       x,
       y,
-      type: char as TileType,
-      isStart: char === "S",
       connections: new Set(connections.map((c) => c.x + "-" + c.y)),
     });
   });
@@ -122,30 +122,28 @@ lines.forEach((line, y) => {
 
 const removeDangling = (m: Map<string, Tile>) => {
   const map = new Map<string, Tile>();
+
   [...m].forEach(([tileCoord, tile]) => {
-    const validConnections = [...tile.connections.values()].filter(
-      (connectionCoord) => {
-        const connectedTile = m.get(connectionCoord);
-        // console.log(connectionCoord, connectedTile);
-        if (!connectedTile) return false;
-        return connectedTile.connections.has(tileCoord);
+    const validConnections: string[] = [];
+    for (const connectionCoord of tile.connections) {
+      if (m.get(connectionCoord)?.connections.has(tileCoord)) {
+        validConnections.push(connectionCoord);
       }
-    );
-    // console.log([...tile.connections.values()]);
+    }
+
     if (validConnections.length === 2) {
       map.set(tileCoord, {
         x: parseInt(tileCoord.split("-")[0]),
         y: parseInt(tileCoord.split("-")[1]),
-        isStart: tile.isStart,
-        type: tile.type,
         connections: new Set(validConnections),
       });
     }
   });
+
   return map;
 };
 
-let map = removeDangling(uncheckedMap);
+let map = uncheckedMap;
 while (true) {
   const newMap = removeDangling(map);
   if (newMap.size === map.size) {
@@ -155,100 +153,33 @@ while (true) {
   }
 }
 
-console.log("removed dangling");
-
-type Line = [
-  {
-    x: number;
-    y: number;
-  },
-  {
-    x: number;
-    y: number;
-  }
-];
-
-const polygonLines: Line[] = [];
-
-[...map.entries()].forEach(([_, tile], i, data) => {
-  tile.connections.forEach((c) => {
-    const [cx, cy] = c.split("-").map(Number);
-    const l: Line = [
-      {
-        x: tile.x,
-        y: tile.y,
-      },
-      {
-        x: cx,
-        y: cy,
-      },
-    ];
-    l.sort((a, b) => {
-      if (a.x - b.x === 0) {
-        return a.y - b.y;
-      }
-      return a.x - b.x;
-    });
-
-    if (
-      !polygonLines.some(
-        (ll) =>
-          ll[0].x === l[0].x &&
-          ll[0].y === l[0].y &&
-          ll[1].x === l[1].x &&
-          ll[1].y === l[1].y
-      )
-    ) {
-      polygonLines.push(l);
-    }
-  });
-  console.log(`generated lines for ${i}/${data.length}`);
-});
-
-type Point = {
-  x: number;
-  y: number;
-};
-
-// https://stackoverflow.com/a/9997374/8715999
-const ccw = (A: Point, B: Point, C: Point) =>
-  (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
-const intersect = (A: Point, B: Point, C: Point, D: Point) => {
-  return ccw(A, C, D) !== ccw(B, C, D) && ccw(A, B, C) !== ccw(A, B, D);
-};
-
 const isInside = (x: number, y: number) => {
   if (map.get(`${x}-${y}`)) {
     return false;
   }
 
-  let hits = new Set<string>();
+  let hitsCount = 0;
   for (let i = x; i < lines[y].length; i++) {
-    for (const p of polygonLines) {
-      if (intersect({ x: i, y }, { x: lines[y].length + 1, y }, p[0], p[1])) {
-        const hit = p.map((p) => `${p.x},${p.y}`).join(" / ");
-        hits.add(hit);
-        break;
-      }
+    const tile = map.get(`${i}-${y}`);
+    if (
+      tile &&
+      [...tile.connections.values()].some(
+        (c) => parseInt(c.split("-")[1]) === y - 1
+      )
+    ) {
+      hitsCount++;
     }
   }
-  if (hits.size % 2 !== 0) {
-    return true;
-  }
 
-  return false;
+  return hitsCount % 2 !== 0;
 };
 
 let count = 0;
-const total = lines.length * lines[0].length;
-let i = 0;
 for (let y = 0; y < lines.length; y++) {
   for (let x = 0; x < lines[y].length; x++) {
     if (isInside(x, y)) {
       count++;
     }
-    i++;
-    console.log(`${i}/${total}`);
   }
 }
 
